c80ad0c912d7a3e267940995514346957e96c200
Lori Tripoli <lori.tripoli@libero.it>
2012-09-17 17:14:15 +0200

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_filter :signed_in_user,
+	        only: [:index, :edit, :update, :destroy, :following, :followers]
   before_filter :correct_user,   only: [:edit, :update]
   before_filter :admin_user,	 only: :destroy
 
@@ -36,7 +37,6 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
   
-  
   def update
     if @user.update_attributes(params[:user])
       flash[:success] = "Profile updated"
@@ -45,6 +45,20 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
   end
   
   private
